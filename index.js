c141b9e70545252db71acc58ecb1046ad02421c9
AllieTennyson <25487910+AllieTennyson@users.noreply.github.com>
2025-05-20 08:54:23 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+// Add the Azure Storage npm package
+const { BlobServiceClient } = require("@azure/storage-blob");
+
+// Add DOM elements for user interaction:
+const createContainerButton = document.getElementById("create-container-button");
+const deleteContainerButton = document.getElementById("delete-container-button");
+const selectButton = document.getElementById("select-button");
+const fileInput = document.getElementById("file-input");
+const listButton = document.getElementById("list-button");
+const deleteButton = document.getElementById("delete-button");
+const status = document.getElementById("status");
+const fileList = document.getElementById("file-list");
+
+const reportStatus = message => {
+    status.innerHTML += `${message}<br/>`;
+    status.scrollTop = status.scrollHeight;
+}
+
+// Add storage account info
+// Update <placeholder> with your Blob service SAS URL string
+const blobSasUrl = "https://storagesamplecreate.blob.core.windows.net/?sv=2024-11-04&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2025-06-15T00:02:27Z&st=2025-05-14T16:02:27Z&spr=https&sig=l87Pad4GRITh7Sl6s4uxZfQmpNv2LWK2HYGtyCtBGAg%3D";
+
+// Create client objects
+// Create a new BlobServiceClient
+const blobServiceClient = new BlobServiceClient(blobSasUrl);
+
+// Create a unique name for the container by 
+// appending the current time to the file name
+const containerName = "container" + new Date().getTime();
+
+// Get a container client from the BlobServiceClient
+const containerClient = blobServiceClient.getContainerClient(containerName);
